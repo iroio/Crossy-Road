a2c916f94defef9d1f7585ef1183e543013eae0a
Gaesalgu <108252520+iroio@users.noreply.github.com>
2026-06-30 04:59:48 +0900

    06.29~30
    - River 게임오버 로직 구현중

--- Assets/Scripts/Map/River/RiverController.cs
+++ Assets/Scripts/Map/River/RiverController.cs
@@ -1,18 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class RiverController : MonoBehaviour
 {
+    [SerializeField] GameObject _splashPrefab;
+
     GameManager _gameManager;
 
+    IEnumerator Sink(Transform target)
+    {
+        Vector3 start = target.position;
+        Vector3 end = start + Vector3.down * 5f;
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * 3f;
+            target.position = Vector3.Lerp(start, end, t);
+            yield return null;
+        }
+    }
 
     // =========================================================
     // 게임오버 여부 확인
     // ========================================================= 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             _gameManager.GameOver();
+
+            // 게임오버 판정 이후 진행할 로직
+
+            // 이펙트 생성
+            Instantiate(_splashPrefab, other.transform.position, _splashPrefab.transform.rotation);
+
+            // 위치 내리기
+            StartCoroutine(Sink(other.transform));
+
+
         }
     }
 
